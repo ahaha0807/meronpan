e40df3f88401610a8cd76e887ddb8479ec0fe362
ahaha0807 <b5331@oic.jp>
2017-06-06 02:04:12 +0900

    Add List Content Template
    - リストの要素のテンプレート部分を定義

---
+++ public/src/compornents/index/Meronpan.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+// import MeronpanThumbnail from './MeronpanThumbnail.jsx'
+// import MeronpanSimpleData from './MeronpanSimpleData.jsx'
+// import ShowDetail from './ShowDetail.jsx'
+
+const Meronpan = React.createClass({
+  render() {
+    return (
+      <li className="index__data">
+        {/*<MeronpanThumbnail img={this.props.image} />*/}
+        {/*<MeronpanSimpleData price={this.props.data.price} visitedDate={this.props.data.visitedDate} shopName={this.props.data.shopName} shopAccess={this.props.data.shopAccess}/>*/}
+        {/*<ShowDetail id={this.props.meronpanId}/>*/}
+      </li>
+    );
+  }
+})
+
+module.exports = Meronpan
+
+/*  @props-doc
+ *  {
+ *    meronpanId: 1,
+ *    image: 'image_url',
+ *    price: 100,
+ *    visitedDate: '1999-01-01',
+ *    shopName: 'shop_name',
+ *    shopAccess: 'nearest_station & time_to_shop'
+ *  }
+ */
